67ffd4734c3be3f15ade8a58052bf21f63d5a931
DermotCanniffe <dermot.canniffe@smartbear.com>
2019-05-16 18:05:22 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd "/c/Users/dermot.canniffe/OneDrive - SmartBear Software, Inc/gitEnv/junitViewer"
+/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe http://localhost:8877
+
+junit-viewer --results=results --port=8877 
